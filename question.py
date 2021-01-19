c9f7aa91b5bb5d37b3429a23b193b9ae062cb240
Jordan Bourne <jordan.bourne@billgo.com>
2021-01-19 13:55:29 -0700

    Created question

---
+++ question.py
@@ -0,0 +1,9 @@
+'''
+Given the payments.csv file which contains a list of 10,000 payments to random business and addresses,
+find unique businesses contained within the payments list, and provide the most common name for the business
+
+Note: Each business can have several distinct addresses.
+It would be helpful to us to be able to be aware of each unique address,
+but also how the addresses are grouped
+'''
+
